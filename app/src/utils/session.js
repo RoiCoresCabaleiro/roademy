32ae378c41109a8e3ede7305b520c84a67956096
Roi <rccabaleiro@esei.uvigo.es>
2025-07-16 18:46:12 +0200

    inicialización y configuración de capacitor y favicons multiplataforma

--- app/src/utils/session.js
+++
@@ -1,8 +0,0 @@
-// src/utils/session.js
-
-export function doLogout() {
-  // Limpia el token de acceso del localStorage
-  localStorage.removeItem('accessToken');
-  // Redirige al login de forma imperativa
-  window.location.href = '/login';
-}
