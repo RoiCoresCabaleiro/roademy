65952ce34da005c409d4e6759b0c6f55d5f3739b
Roi <rccabaleiro@esei.uvigo.es>
2025-07-22 16:16:55 +0200

    mas cambios hosting

--- server/src/config/sequelize.js
+++ server/src/config/sequelize.js
@@ -3,14 +3,21 @@
 //require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
+const url = process.env.MYSQL_URL;
+if (!url) {
+  console.error('❌ ERROR: no se encontró MYSQL_URL en las variables de entorno');
+  process.exit(1);
+}
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  url,
+  //process.env.DB_NAME,
+  //process.env.DB_USER,
+  //process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
+    //host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.DB_PORT || 3306,
+    //port: process.env.DB_PORT || 3306,
     timezone: "+02:00",
     logging: false,
     define: {
