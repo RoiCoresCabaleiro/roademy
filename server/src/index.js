de60259caa009b5b315434bb3db95889111857dc
Roi <rccabaleiro@esei.uvigo.es>
2025-07-25 19:50:56 +0200

    Prueba 5: Unificación dev y prod

--- server/src/index.js
+++ server/src/index.js
@@ -1,28 +1,23 @@
 // server/src/index.js
 
-//if (process.env.NODE_ENV !== 'prod') {
-//  require('dotenv').config(); 
-//}
+//require("dotenv").config();
 const { sequelize } = require("./models");
 const { seedData } = require("./seed");
 const app = require("./app");
 require("./cron/cleanupRefreshTokens");
 
-const PORT = 3000;  // const PORT = process.env.PORT || 3000;
+const PORT = 3000; // const PORT = process.env.PORT || 3000;
 
 async function start() {
   try {
-    await sequelize.sync();  // en desarrollo usar "{ alter: true }" y en produción usar migraciones
+    await sequelize.sync(); // en desarrollo usar "{ alter: true }" y en produción usar migraciones
 
     // Sembrar datos iniciales si no se ha hecho ya
     const { Tema } = require("./models");
-    const count = await Tema.count();
-    if (count === 0) {
-      await seedData();
-    }
+    if ((await Tema.count()) === 0) await seedData();
 
     app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
+      console.log(`Server listening on port ${PORT} : MODE: (${process.env.NODE_ENV || 'development'})`);
     });
   } catch (err) {
     console.error("Error starting server:", err);
