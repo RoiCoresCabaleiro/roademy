a2106bff9c0d8f84efe626f9b3e37227f29f48fb
Roi <rccabaleiro@esei.uvigo.es>
2025-08-30 16:20:25 +0200

    eliminar ghpages 1

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./", // base: "/roademy/", // para gh-pages
+  base: "./", // base: "/roademy/"
   build: {
     outDir: "dist",
   },
